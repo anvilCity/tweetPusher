3ca8dcca7f66cde33f627c946f21d41e43fae2af
James Frost <hello@jamesfrost.me>
2016-05-08 10:54:04 +0100

    Changed update interval

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const _pusher = require('./src/pusher.js');
-const _updateInterval = 10; // seconds
+const _updateInterval = 4; // seconds
+
 _pusher.pushUpdate();
 setInterval( _pusher.pushUpdate, _updateInterval*1000 );
